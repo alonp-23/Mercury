630634bc6306bbdd0d34604e82f3c647c843123f
hackathon IDF <hack@cs.colman.ac.il>
2021-06-06 19:22:47 +0300

    feat(Bar.vue): basic bar graph

---
+++ mercury-project/src/views/Bar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>Bar Chart Example in Vue</h3>
+    <bar-chart></bar-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from '@/components/BarChart'
+
+export default {
+  components: {
+    BarChart
+  }
+}
+</script>
