bdf4bb261fade58e69d579915099e341f12a118c
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 13:43:37 +0300

    Feat(InteList): list in scrollable

--- mercury-project/frontend/src/components/InteList.vue
+++ mercury-project/frontend/src/components/InteList.vue
@@ -1,79 +1,80 @@
 <template>
 <div>
-     <v-card max-width="500" class="mx-auto">
-    <v-toolbar  dark>
-      <v-toolbar-title>ציר מודיעיני</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
-    
-    <v-list>
-        <v-chip class="mr-2" @click="show_wanted = false" label>
-        <v-icon left v-if="!show_wanted">mdi-checkbox-marked-circle</v-icon>                       
-        חשודים
-        </v-chip>
-        <v-chip class="mr-2" @click="show_wanted = true" label> 
-        <v-icon left v-if="show_wanted">mdi-checkbox-marked-circle</v-icon>            
-        מבוקשים
-        </v-chip>      
-      <v-list-item
-        v-for="person in pplList"
-        :key="person.name"
-      >
-      <v-list-item-avatar>
-          <v-img :src="avatar(person)"></v-img>
-        </v-list-item-avatar>
-        <v-list-item-content>
-            <v-list-item-title v-text="full_name(person.first_name,person.last_name)" ></v-list-item-title>
-          <v-list-item-subtitle v-text="person.id"></v-list-item-subtitle>
-        </v-list-item-content>
+  <v-card class="mx-auto" :max-width=width>
+    <v-card-title class="white--text black">
+      ציר מודיעיני
+      <v-spacer></v-spacer>      
+    </v-card-title>   
+    <v-divider></v-divider>
+     <v-chip class="mr-2 ma-3" @click="show_wanted = false" label>
+            <v-icon left v-if="!show_wanted">mdi-checkbox-marked-circle</v-icon>                       
+            חשודים
+            </v-chip>
+        <v-chip class="mr-2 ma-3" @click="show_wanted = true" label> 
+            <v-icon left v-if="show_wanted">mdi-checkbox-marked-circle</v-icon>            
+            מבוקשים
+        </v-chip>  
+    <v-virtual-scroll
+      :items="pplList"
+      :item-height="50"
+      :height=height
+    >
+      <template v-slot:default="{ item }">
+        <v-list-item>
+          <v-list-item-avatar>
+                <v-img :src="avatar(item)"></v-img>
+            </v-list-item-avatar>
 
-          <v-dialog v-model="dialog" width="500">
-      
-    <template v-slot:activator="{ on, attrs }">        
-        <v-list-item-action 
-          v-bind="attrs"
-          v-on="on">
-          <v-btn icon>
-            <v-icon color="grey lighten-1" >mdi-arrow-left</v-icon>
-          </v-btn>
-        </v-list-item-action>
+          <v-list-item-content>
+                <v-list-item-title v-text="full_name(item.first_name,item.last_name)" ></v-list-item-title>
+                <v-list-item-subtitle v-text="item.id"></v-list-item-subtitle>
+            </v-list-item-content>
+
+          <v-dialog v-model="dialog" width="500">      
+                <template v-slot:activator="{ on, attrs }">        
+                    <v-list-item-action 
+                        v-bind="attrs"
+                        v-on="on">
+                        <v-btn icon>
+                            <v-icon color="grey lighten-1" >mdi-arrow-left</v-icon>
+                        </v-btn>
+                    </v-list-item-action>
+                </template>
+                <v-card>
+
+                    <v-card-title class="headline grey lighten-2">
+                    {{full_name(item.first_name,item.last_name)}}
+                    </v-card-title>
+
+                    <v-img 
+                        contain
+                        :src="avatar(item)"
+                        max-height="150" 
+                        class="ma-3"/>          
+
+                    <v-card-text>          
+                    תעודת זהות: {{item.id}} <br>
+                    כתובת: {{item.address}} <br>
+                    טלפון: {{item.phone_number}}
+                    </v-card-text>
+
+                    <v-divider></v-divider>
+
+                    <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="primary"
+                        text
+                        @click="dialog = false"
+                    >
+                        סגור
+                    </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-list-item>
       </template>
-      <v-card>
-        <v-card-title class="headline grey lighten-2">
-          {{full_name(person.first_name,person.last_name)}}
-        </v-card-title>
-
-        <v-img 
-            contain
-            :src="avatar(person)"
-            max-height="150" 
-            class="ma-3"/>          
-
-        <v-card-text>          
-          תעודת זהות: {{person.id}} <br>
-          כתובת: {{person.address}} <br>
-          טלפון: {{person.phone_number}}
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
-          >
-            סגור
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-
-        
-      </v-list-item>
-    </v-list>
-    
+    </v-virtual-scroll>
   </v-card>
 </div>
     
@@ -82,6 +83,14 @@
 <script>
 export default {
   name: 'InteList',
+  props: {
+      width: {
+          default: 500
+      },
+      height: {
+          default: 500
+      }
+  },
   data: ()=> {
     return {
       pplOfIntrest: [{first_name: "ישראל", last_name:"ישראלי", id: 456346, 
@@ -90,20 +99,21 @@ export default {
       {first_name: "שאול", last_name:"שאולה" , id: 3452345, person_img_url: "https://www.pngarts.com/files/11/Avatar-Free-PNG-Image.png", 
       is_wanted: true, phone_number:1234567, address:"דרדר 51, מצפה נשר"}],
       dialog: false,
-      show_wanted:false
+      show_wanted:false,      
     }
   },
   computed: {    
     pplList: function () {
       return this.pplOfIntrest.filter(person=> person.is_wanted && this.show_wanted || !person.is_wanted && !this.show_wanted)
-    },
-      
+    }      
   },
   methods: {
       full_name: function(first_name,last_name){return `${first_name} ${last_name}`},
-      avatar: function(person){return person.person_img_url}  
-  }
-  
+      avatar: function(person){return person.person_img_url},
+      genRandomIndex (length) {
+        return Math.ceil(Math.random() * (length - 1))
+      }
+  }  
 };
 </script>
 <style >
