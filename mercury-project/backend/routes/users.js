ad99d77fcc0bf818bde57f410d138e14df05ff18
alonp-23 <85433851+alonp-23@users.noreply.github.com>
2021-06-07 19:10:25 +0300

    add login into main
    Co-Authored-By: alonp-23 <85433851+alonp-23@users.noreply.github.com>

---
+++ mercury-project/backend/routes/users.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const userService = require('../services/UsersService.js');
+
+router.get('/allUsers', async function(req,res,next) {
+  res.json(await userService.getAll());
+});
+
+router.post('/login', async function(req,res,next) {
+  const { username, password} = req.body;
+  const loginSuccessful = await userService.authenticateUser(username, password);
+  
+  if(loginSuccessful) { res.send(200);} 
+  else { res.send(401); }
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@
+  ${process.env.DB_HOST}:31000/${process.env.DB_DATABASE}`);
+});
+
+module.exports = router;
