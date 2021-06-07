c319e5892da926528344eed6dece47fa16b28c51
avijs332 <hack@cs.colman.ac.il>
2021-06-07 19:46:37 +0300

    feat(weatherCard): added basic weather card funcionality

--- mercury-project/frontend/src/views/Home.vue
+++ mercury-project/frontend/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
+<div>
   <Login/>
+  <Weather/>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Login from '@/components/Login.vue';
+import Weather from '@/components/WeatherCube.vue';
 
 export default {
   name: 'Home',
   components: {
     Login,
+    Weather,
   },
 };
 </script>
