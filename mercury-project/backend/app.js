374266bbf113459698fa75feedbe1c198799ab66
avijs332 <hack@cs.colman.ac.il>
2021-06-07 17:43:50 +0300

    feat(): pushing working prod v1.0.0

--- mercury-project/backend/app.js
+++ mercury-project/backend/app.js
@@ -15,6 +15,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
