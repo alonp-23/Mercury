87ebd7650cbf9ee731a8a5635d6ff7baeb96079e
Sean <59194895+avijs332@users.noreply.github.com>
2021-06-07 17:01:58 +0300

    Update vuetify.js

--- mercury-project/frontend/src/plugins/vuetify.js
+++ mercury-project/frontend/src/plugins/vuetify.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
@@ -7,7 +6,6 @@ Vue.use(Vuetify);
 export default new Vuetify({
     rtl: true
 });
-=======
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 
@@ -16,4 +14,3 @@ Vue.use(Vuetify)
 export default new Vuetify({
   rtl: true,
 })
->>>>>>> 6033b8a33227e021b9da72d6cdcbd63ff55daab4
