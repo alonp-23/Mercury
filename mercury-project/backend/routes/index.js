0482a72a2ad4b422841a3b5a718110682d79eb33
Sean <59194895+avijs332@users.noreply.github.com>
2021-06-08 19:31:51 +0300

    Update index.js

--- mercury-project/backend/routes/index.js
+++ mercury-project/backend/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -41,22 +42,32 @@ writeToJson(filename, content){
 
 /* GET home page. */
 router.get('/weather', function(req, res, next) {
-    
+    res.send(readFromFile('weather.json'));
 });
 
 /* GET home page. */
 router.get('/holidaysIL', function(req, res, next) {
-  
+    res.send(readFromFile('holidaysIL.json'));
 });
 
 /* GET home page. */
 router.get('/holidaysUS', function(req, res, next) {
-  
+    res.send(readFromFile('holidaysUS.json'));
 });
 
 /* GET home page. */
 router.get('/holidaysJO', function(req, res, next) {
-  
+    res.send(readFromFile('holidaysJO.json'));
 });
+
+readFromFile(filename){
+  fs.readFile(filename, 'utf8', (err, jsonString) => {
+      if (err) {
+          console.log("File read failed:", err)
+          return '';
+      }
+      return jsonString;
+  })
+}
 
 module.exports = router;
