84afff4437cd83217beada04b874a1586d7a2a4b
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 11:02:52 +0300

    feat(): connected chart to data

--- mercury-project/frontend/src/components/pieChartCard.vue
+++ mercury-project/frontend/src/components/pieChartCard.vue
@@ -1,30 +1,36 @@
 <template>
-  <div>
-    <v-card class="mx-auto" :max-width="width" elevation="2">
-      <v-card-title class="white--text" id="title">
-        סטטוס
-        <v-spacer></v-spacer>
-      </v-card-title>
-      <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
-    </v-card>
-  </div>
+    <div>
+        <v-card class="mx-auto" :max-width=width elevation="2" >
+            <v-card-title class="white--text" id="title">
+                סטטוס
+            <v-spacer></v-spacer>      
+            </v-card-title> 
+            <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
+        </v-card>
+    </div>    
 </template>
 
 <script>
 import PieChart from "./pieChart.js";
-import { mapState } from "vuex";
-import { mapMutations } from 'vuex'
-export default {
-  name: "pieChartCard",
-  components: {
-    PieChart
-  },
-  props: {
-    width: {
-      default: 400
-    }
-  },
-  data() {
+export default {    
+    name: 'pieChartCard',
+    components:{
+        PieChart
+    },
+    props: {
+      width: {
+          default: 400
+      },
+      suspects: {
+          type: Number,
+          default: 21
+      },
+      wanted: {
+          type: Number,
+          default: 21
+      },
+    },
+    data() {
     return {
       chartOptions: {
         hoverBorderWidth: 20
@@ -37,34 +43,25 @@ export default {
           {
             label: "Data One",
             backgroundColor: ["#141d33", "#283965"],
-            data: [this.suspectsNo, this.wantedNo]
+            data: [parseInt(this.suspects.toString()), parseInt(this.wanted.toString())]
           }
         ]
       }
     };
-  },
-  computed: {
-    ...mapState(["pplOfIntrest","suspectNo","wantedNo"])
-  },
-  methods:{
-      ...mapMutations(["initPplArr"]),
-  },
-  mounted() {
-    this.initPplArr();
-  },
-};
+  }
+}
 </script>
 
 <style scoped>
-#title {
-  background-color: #141d33;
-}
-div {
-  font-family: Calibri, Candara, Segoe, Segoe UI, Optima, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #title{
+      background-color: #141d33
+    }
+    div {
+        font-family: Calibri,Candara,Segoe,Segoe UI,Optima,Arial,sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- mercury-project/frontend/src/views/About.vue
+++ mercury-project/frontend/src/views/About.vue
@@ -1,24 +1,40 @@
 <template>
   <div class="about">
-    <iteList/>
-    <pieChartCard/>
+    <pieChartCard :suspects="this.pplOfIntrest.filter(person=> !person.is_wanted).length" :wanted="this.pplOfIntrest.filter(person=> person.is_wanted).length"/>
   </div>
 </template>
 
 <script>
 import pieChartCard from '@/components/pieChartCard.vue';
 import iteList from '@/components/InteList.vue'
-import { mapMutations } from 'vuex'
 export default {
   name: 'Home',
   components: {
     pieChartCard,iteList
   },
   methods:{
-      ...mapMutations(["initPplArr"]),
+  },
+  data: ()=> {
+    return {
+      pplOfIntrest: [],
+      dialog: false,
+      show_wanted:false,      
+    }
   },
   mounted() {
     this.initPplArr();
   },
+  methods:{
+    initPplArr(){
+      //api
+      this.pplOfIntrest =[{first_name: "ישראל", last_name:"ישראלי", id: 456346, 
+      person_img_url: "https://ps.w.org/ultimate-member/assets/icon-256x256.png?rev=2143339", 
+      is_wanted: false, phone_number:1234567, address:"דרדר 51, מצפה נשר"},
+      {first_name: "שאול", last_name:"שאולה" , id: 3452345, person_img_url: "https://www.pngarts.com/files/11/Avatar-Free-PNG-Image.png", 
+      is_wanted: true, phone_number:143234, address:"דרדר 51, מצפה נשר"},
+      {first_name: "דובים", last_name:"ביער" , id: 3452345, person_img_url: "https://www.pngarts.com/files/11/Avatar-Free-PNG-Image.png", 
+      is_wanted: true, phone_number:241324, address:"דרדר 51, מצפה נשר"}]
+    }
+  }
 };
 </script>
