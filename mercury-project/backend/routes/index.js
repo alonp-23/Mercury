99e344ebe85683bd0d4db14cea63ce78dbec17d6
Sean <59194895+avijs332@users.noreply.github.com>
2021-06-08 19:11:32 +0300

    Update index.js

--- mercury-project/backend/routes/index.js
+++ mercury-project/backend/routes/index.js
@@ -7,22 +7,22 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/weather', function(req, res, next) {
+router.post('/weather', function(req, res, next) {
     writeToJson('weather.json', req.body);
 });
 
 /* GET home page. */
-router.get('/holidaysIL', function(req, res, next) {
+router.post('/holidaysIL', function(req, res, next) {
   writeToJson('holidaysIL.json', req.body);
 });
 
 /* GET home page. */
-router.get('/holidaysUS', function(req, res, next) {
+router.post('/holidaysUS', function(req, res, next) {
   writeToJson('holidaysUS.json', req.body);
 });
 
 /* GET home page. */
-router.get('/holidaysJO', function(req, res, next) {
+router.post('/holidaysJO', function(req, res, next) {
   writeToJson('holidaysJO.json', req.body);
 });
 
