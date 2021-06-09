2f95431dd0bd14bb796fa9d96f56340977c7e861
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 17:48:29 +0300

    Merge()

--- mercury-project/frontend/src/components/PersonDetails.vue
+++ mercury-project/frontend/src/components/PersonDetails.vue
@@ -1,142 +1,120 @@
 <template>
-    <v-card
-    :color="bgColor"
-    dark
-    class="mx-auto rounded-card"
-    width="56vw"
-    outlined
-  >
-  <v-container>
+  <v-card color="#141d33" dark class="mx-auto rounded-card" width="90vw" outlined>
+    <v-container>
       <v-row>
-          <v-col>
-    <v-card-actions>
-        <v-container v-if="wantedButtonStatus">
-            <v-row>
-                <h1 class="text-center mx-auto">{{lock_title}}</h1>
-            </v-row>
-            <v-row>
+        <v-col>
+          <v-card-actions>
+            <v-container>
+              <v-row>
+                <h1 class="text-center mx-auto">{{ lock_title }}</h1>
+              </v-row>
+              <v-row>
                 <div class="text-center mx-auto">
-                    <v-btn 
-                        outlined
-                        rounded
-                        text
-                        x-large
-                        icon
-                        @click="updateWantedState"
-                    >
-                        <v-icon :color ="state_color" :class="lock_icon"></v-icon>
-                    </v-btn>
+                  
                 </div>
-            </v-row>
-        </v-container>
-    </v-card-actions>
-    </v-col>
-          <v-col>
-    <v-list-item two-line>
-      <v-list-item-content>
-        <v-list-item-title class="text mb-1 text-lg-right">
-            <strong>פלאפון</strong>
-          {{suspectDetails.person.phoneNumber}}
-        </v-list-item-title>
-        <div class="text-overline-h4 mb-4 text-lg-right">
-          <strong>כתובת </strong>
-          {{suspectDetails.person.address}}
-        </div>
-        
-      </v-list-item-content>
+              </v-row>
+            </v-container>
+          </v-card-actions>
+        </v-col>
+        <v-col>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title class="text mb-1 text-lg-right">
+                <strong>פלאפון</strong>
+                {{ suspectDetails.person.phoneNumber }}
+              </v-list-item-title>
+              <div class="text-overline-h4 mb-4 text-lg-right">
+                <strong>כתובת </strong>
+                {{ suspectDetails.person.address }}
+              </div>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+        <v-col>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <div class="text-overline-h4 mb-4 text-lg-right">
+                <strong>{{
+                  suspectDetails.person.firstName + " " + suspectDetails.person.lastName
+                }}</strong>
+              </div>
+              <v-list-item-title class="text mb-1 text-lg-right">
+                {{ suspectDetails.personId }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
 
-      
-    </v-list-item>
-    
-    </v-col>
-      <v-col>
-    <v-list-item two-line>
-      <v-list-item-content>
-        <div class="text-overline-h4 mb-4 text-lg-right">
-          <strong>{{suspectDetails.person.firstName + " " + suspectDetails.person.lastName}}</strong>
-        </div>
-        <v-list-item-title class="text mb-1 text-lg-right">
-          {{suspectDetails.personId}}
-        </v-list-item-title>
-      </v-list-item-content>
-
-      
-    </v-list-item>
-    
-    </v-col>
-    
-    <v-col>
-        <v-list-item-avatar
-        tile
-        size="120"
-        color="grey"
-        class=" mx-auto"
-      ></v-list-item-avatar>
-    </v-col>
-
-    </v-row>
+        <v-col>
+          <v-list-item>
+            <v-avatar size="120" class=" mx-auto"
+              ><img :src="suspectDetails.person.personImageURL" alt="John"
+            /></v-avatar>
+          </v-list-item>
+        </v-col>
+      </v-row>
     </v-container>
   </v-card>
 </template>
 
 <script>
 import api from "../api/api";
-
 export default {
-    data () {
-        return { 
-            suspectDetails : {}
-        }
+  data() {
+    return {
+      suspectDetails: {}
+    };
+  },
+  async mounted() {
+    const response = await api.profile().getProfileById(this.personId);
+    this.suspectDetails = response.data;
+  },
+  async updated() {
+    const response = await api.profile().getProfileById(this.personId);
+    this.suspectDetails = response.data;
+  },
+  props: {
+    bgColor: {
+      type: String,
+      required: true
     },
-    async mounted() {
-        const response = await api.profile().getProfileById(this.personId);
-        this.suspectDetails = response.data;
+    wantedButtonStatus: {
+      type: Boolean,
+      required: true
     },
-    props: {
-        bgColor: {
-            type: String,
-            required: true
-        },
-        wantedButtonStatus: {
-            type: Boolean,
-            required: true
-        },
-        personId: {
-            type: String,
-            required: true    
-        },
-    },
-    computed: {
-        state_color : function () {
-            return this.suspectDetails.wanted ? 'red' : 'green';
-        },
-        lock_icon : function () {
-            return this.suspectDetails.wanted ? 'fas fa-lock' : 'fas fa-lock-open';
-        },
-        lock_title : function () {
-            return this.suspectDetails.wanted ? 'מבוקש' : 'חשוד';
-        }
-    },
-    methods:{
-      updateWantedState: function(){
-          
-         this.$emit("change-wanted-state") ;
-      }
+    personId: {
+      type: String,
+      required: true
     }
-}
+  },
+  computed: {
+    state_color: function() {
+      return this.suspectDetails.wanted ? "red" : "green";
+    },
+    lock_icon: function() {
+      return this.suspectDetails.wanted ? "fas fa-lock" : "fas fa-lock-open";
+    },
+    lock_title: function() {
+      return this.suspectDetails.wanted ? "מבוקש" : "חשוד";
+    }
+  },
+  methods: {
+    updateWantedState: function() {
+      this.$emit("change-wanted-state");
+    }
+  }
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Heebo&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Heebo&display=swap");
 * {
-    font-family: 'Heebo', sans-serif !important;
+  font-family: "Heebo", sans-serif !important;
 }
-.rounded-card{
-    border-radius:10px;
+.rounded-card {
+  border-radius: 10px;
 }
-
 .text {
-  font-family: 'Heebo', sans-serif !important;
+  font-family: "Heebo", sans-serif !important;
 }
 </style>
--- mercury-project/frontend/src/components/ProfilePage.vue
+++ mercury-project/frontend/src/components/ProfilePage.vue
@@ -1,62 +1,75 @@
 <template>
-<v-container>
+  <v-container>
     <v-row>
-        <person-details :personId="$route.params.id" @change-wanted-state="changeWantedState" bgColor="#2A2B38" :wantedButtonStatus="wantedButton"></person-details>
+      <person-details
+        v-bind:personId="this.personId"
+        @change-wanted-state="changeWantedState"
+        bgColor="#141d33"
+        :wantedButtonStatus="wantedButton"
+      ></person-details>
     </v-row>
     <v-row class="mt-3">
-        <v-col cols="2">
-        </v-col>
-        <v-col
-        cols="2">
-            <titled-info bgColor="#2A2B38" amountColor="#FFAC04" :amount="posts" title="מספר פוסטים בשבוע אחרון"></titled-info>
-        </v-col>
-        <v-col cols="2">
-            <driver-license :personId="$route.params.id" ></driver-license>
-        </v-col>
-        <v-col cols="4">
-            <reports :personId="$route.params.id"></reports>
-        </v-col>
-        <v-col cols="2">
-        </v-col>
+      <v-col cols="2"> </v-col>
+      <v-col cols="2">
+        <titled-info
+          bgColor="#141d33"
+          amountColor="#FFAC04"
+          :amount="posts"
+          title="מספר פוסטים בשבוע אחרון"
+        ></titled-info>
+      </v-col>
+      <v-col cols="2">
+        <driver-license v-bind:personId="this.personId"></driver-license>
+      </v-col>
+      <v-col cols="4">
+        <reports v-bind:personId="this.personId"></reports>
+      </v-col>
+      <v-col cols="2"> </v-col>
     </v-row>
-</v-container>
+  </v-container>
 </template>
 
 <script>
-import PersonDetails from '../components/PersonDetails.vue';
-import TitledInfo from '../components/TitledInfo.vue';
+import PersonDetails from "../components/PersonDetails.vue";
+import HeatMap from "../components/HeatMap.vue";
+import TitledInfo from "../components/TitledInfo.vue";
 import Reports from "../components/reports.vue";
 import driverLicense from "../components/driverLicense.vue";
 import api from "../api/api";
-
 export default {
-    name: 'ProfilePage',
-    components: {
-        PersonDetails,
-        TitledInfo,
-        Reports,
-        driverLicense
-    },
-    methods : {
-      changeWantedState : function () {
-        this.person.wanted = !this.person.wanted
-      }
-    },
-    async mounted() {
-        const response = await api.profile().getNumOfPostsById(this.$route.params.id);
-        this.posts = response.data;
-    },
-    data() {
-        return {
-        posts: 0,
-        wantedButton: true
-        } 
+  name: "ProfilePage",
+  components: {
+    PersonDetails,
+    TitledInfo,
+    Reports,
+    driverLicense,
+    HeatMap
+  },
+  methods: {
+    changeWantedState: function() {
+      this.person.wanted = !this.person.wanted;
     }
-}
+  },
+  async mounted() {
+    const response = await api.profile().getNumOfPostsById(this.personId);
+    this.posts = response.data;
+  },
+  async updated() {
+    const response = await api.profile().getNumOfPostsById(this.personId);
+    this.posts = response.data;
+  },
+  data() {
+    return {
+      posts: 0,
+      wantedButton: false
+    };
+  },
+  props: ["personId"]
+};
 </script>
 
 <style scoped>
-.row{
-    padding: 10px
+.row {
+  padding: 10px;
 }
 </style>
